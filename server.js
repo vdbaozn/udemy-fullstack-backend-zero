d84b5fae7b08d62c3db055553f81dc9b2ad2cdfd
Vo Duy Bao <vdbao.zn@gmail.com>
2024-07-10 10:07:45 +0700

    bai 17

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
+const express = require('express') //import express
 
-//Load HTTP module (thư viện đã có sẵn khi cài đặt node.js)
-const http = require("http");
-const hostname = "localhost"; //cái này === http://localhost
-const port = 3000;
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-    //Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World\n Vo Duy Bao");
-});
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-//tự động chạy trên localhost
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const app = express() // tạo express application
+const port = 8080  // init port
+//khai báo routes
+//req (request), res(response) là 2 object trong môi trường Node.js
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+    res.send('Check ABC')
+})
+//run server trên port đã khởi tạo trước đấy
+//nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
